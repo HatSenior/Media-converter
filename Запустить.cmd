ee5b5ca01655c151cf0b05b1de488b33138f3cd6
HatSenior <ruslanver12@gmail.com>
2026-08-13 00:16:05 +0300

    Upgrade

--- Запустить.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if exist "%~dp0Media Forge.exe" (
-    start "" "%~dp0Media Forge.exe"
-    exit /b
-)
-start "Media Forge" powershell.exe -STA -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "try{$env:MEDIAFORGE_ROOT='%~dp0';$code=[IO.File]::ReadAllText('%~dp0MediaForge2.ps1',[Text.Encoding]::UTF8);[scriptblock]::Create($code).Invoke()}catch{Add-Type -AssemblyName PresentationFramework;[Windows.MessageBox]::Show($_.Exception.Message,'Media Forge — ошибка запуска')}"
